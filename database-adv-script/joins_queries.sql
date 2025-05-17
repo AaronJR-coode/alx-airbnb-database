a25ecb6ec5825b95f58234c90745bae96e9f491d
AaronJR-coode <khombeaaronjunior@gmail.com>
2025-05-18 01:11:24 +0200

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -10,3 +10,19 @@ SELECT users.user_id, users.username, bookings.booking_id, bookings.booking_date
 FROM users
 FULL OUTER JOIN bookings ON users.user_id = bookings.user_id;
 
+SELECT property_id, property_name
+FROM properties
+WHERE property_id IN (
+    SELECT property_id
+    FROM reviews
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
+);
+
+SELECT u.user_id, u.username
+FROM users u
+WHERE (
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.user_id
+) > 3;
