882eadc951b2758245ecde1b5e2568cf4db47e37
AaronJR-coode <khombeaaronjunior@gmail.com>
2025-05-18 01:15:42 +0200

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,1 +1,14 @@
+SELECT u.user_id, u.username
+FROM users u
+WHERE (
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.user_id
+) > 3;
+
+SELECT u.user_id, u.username
+FROM users u
+JOIN bookings b ON u.user_id = b.user_id
+GROUP BY u.user_id, u.username
+HAVING COUNT(b.booking_id) > 3;
 
